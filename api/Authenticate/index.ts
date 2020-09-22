cce2261f4670325889075341daab2fc7cb01b970
Ben Bristow <ben@bristow.me>
2020-09-23 00:11:07 +0100

    Remove debug

--- api/Authenticate/index.ts
+++ api/Authenticate/index.ts
@@ -3,19 +3,11 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { getSpotifyAuthorizeUrl } from '../lib/authenticationHelper';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    try {
-        const authorizeUrl = getSpotifyAuthorizeUrl();
+    const authorizeUrl = getSpotifyAuthorizeUrl();
 
-        context.res = {
-            body: {
-                url: authorizeUrl
-            }
-        }
-    } catch (ex) {
-        context.res = {
-            body: {
-                error: JSON.stringify(ex)
-            }
+    context.res = {
+        body: {
+            url: authorizeUrl
         }
     }
 };
