552aee42a28194d7e8031fff9d0c21586059521a
Ben Bristow <ben@bristow.me>
2020-09-22 22:13:50 +0100

    Revert "Test"
    This reverts commit ca086b7db9175979b041e8e2ff8305d648be8c3e.

--- api/Authenticate/index.ts
+++ api/Authenticate/index.ts
@@ -3,11 +3,11 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { getSpotifyAuthorizeUrl } from '../lib/authenticationHelper';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    // const authorizeUrl = getSpotifyAuthorizeUrl();
+    const authorizeUrl = getSpotifyAuthorizeUrl();
 
     context.res = {
         body: {
-            url: 'test'
+            url: authorizeUrl
         }
     }
 };
