7d7d9e54df0045bdaf88d4fc65365371add25e1b
Ben Bristow <ben@bristow.me>
2020-10-04 18:59:36 +0100

    Debug

--- api/AuthenticateCallback/index.ts
+++ api/AuthenticateCallback/index.ts
@@ -1,19 +1,16 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import StatusCode from "status-code-enum";
 
-import { getSpotifyToken } from "../lib/authenticationHelper";
-import { createJwtToken } from "../lib/tokenHelper";
-
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const spotifyToken = await getSpotifyToken(req.query.code);
-    const jwtToken = createJwtToken(spotifyToken);
+    // const spotifyToken = await getSpotifyToken(req.query.code);
+    // const jwtToken = createJwtToken(spotifyToken);
 
     context.res = {
         status: StatusCode.RedirectFound,
         headers: {
-            location: `${process.env.FRONTEND_URL}/?token=${jwtToken}`
+            location: 'https://www.google.com/'
         }
-    };
+    }
 };
 
 export default httpTrigger;
