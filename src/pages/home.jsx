230799821d09a186d4b42c797801bfb4682c8d1e
Ben Bristow <ben@bristow.me>
2020-11-09 14:10:05 +0000

    Currently broken message

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+// import React, { useState } from 'react';
 
-import { getAuthenticateUrl } from "../apiClient";
+// import { getAuthenticateUrl } from "../apiClient";
 
-import { showError } from "../helpers/messageHelper";
+// import { showError } from "../helpers/messageHelper";
 import { authenticated } from '../helpers/authenticationHelper';
 
-import { Button } from '../components/button';
+// import { Button } from '../components/button';
 
 export const HomePage = () => {
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
   if (authenticated()) {
     window.location.reload();
     return null;
   }
 
-  const startAuth = async () => {
-    setLoading(true);
+  // const startAuth = async () => {
+  //   setLoading(true);
 
-    try {
-      const authenticateUrl = await getAuthenticateUrl();
-      window.location.href = authenticateUrl;
-    } catch {
-      showError('Unable to get Spotify authentication URL. Please check your connection and try again');
-      setLoading(false);
-    }
-  };
+  //   try {
+  //     const authenticateUrl = await getAuthenticateUrl();
+  //     window.location.href = authenticateUrl;
+  //   } catch {
+  //     showError('Unable to get Spotify authentication URL. Please check your connection and try again');
+  //     setLoading(false);
+  //   }
+  // };
 
   return (
     <div className="page page--home">
@@ -34,7 +35,10 @@ export const HomePage = () => {
         <p className="lead">
           Auto-magically generate a Spotify Playlist with songs from bands of your upcoming gigs via your Songkick calendar
         </p>
-        <Button theme="spotify" icon="spotify" size="lg" loading={loading} onClick={startAuth}>Login with Spotify</Button>
+        <div className="alert alert-danger">
+          Currently broken due to Azure updates. See <a href="https://github.com/Azure/static-web-apps/issues/165" target="_blank" rel="noopener noreferrer">GitHub</a> for more info.
+        </div>
+        {/* <Button theme="spotify" icon="spotify" size="lg" loading={loading} onClick={startAuth}>Login with Spotify</Button> */}
       </div>
     </div>
   );
