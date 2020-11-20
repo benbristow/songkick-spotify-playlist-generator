2a65665b97a21b5a13f61ad79e9016c9ffd53c08
Ben Bristow <ben@bristow.me>
2020-11-20 17:43:05 +0000

    Think it works again now

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// import { getAuthenticateUrl } from "../apiClient";
-
-// import { showError } from "../helpers/messageHelper";
+import { getAuthenticateUrl } from "../apiClient";
+import { showError } from "../helpers/messageHelper";
 import { authenticated } from '../helpers/authenticationHelper';
-
-// import { Button } from '../components/button';
+import { Button } from '../components/button';
 
 export const HomePage = () => {
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   if (authenticated()) {
     window.location.reload();
     return null;
   }
 
-  // const startAuth = async () => {
-  //   setLoading(true);
+  const startAuth = async () => {
+    setLoading(true);
 
-  //   try {
-  //     const authenticateUrl = await getAuthenticateUrl();
-  //     window.location.href = authenticateUrl;
-  //   } catch {
-  //     showError('Unable to get Spotify authentication URL. Please check your connection and try again');
-  //     setLoading(false);
-  //   }
-  // };
+    try {
+      const authenticateUrl = await getAuthenticateUrl();
+      window.location.href = authenticateUrl;
+    } catch {
+      showError('Unable to get Spotify authentication URL. Please check your connection and try again');
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="page page--home">
@@ -38,7 +36,7 @@ export const HomePage = () => {
         <div className="alert alert-danger">
           Currently broken due to Azure updates. See <a href="https://github.com/Azure/static-web-apps/issues/165" target="_blank" rel="noopener noreferrer">GitHub</a> for more info.
         </div>
-        {/* <Button theme="spotify" icon="spotify" size="lg" loading={loading} onClick={startAuth}>Login with Spotify</Button> */}
+        <Button theme="spotify" icon="spotify" size="lg" loading={loading} onClick={startAuth}>Login with Spotify</Button>
       </div>
     </div>
   );
