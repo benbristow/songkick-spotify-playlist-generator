46d832060e5e21361298455a6de1411223f44861
Ben Bristow <ben@bristow.me>
2021-06-06 18:10:25 +0100

    fix: References

--- api/Services/AuthenticationService.cs
+++ api/Services/AuthenticationService.cs
@@ -4,7 +4,6 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Tokens;
-using SongkickSpotifyPlaylistGenerator.Functions;
 using SongkickSpotifyPlaylistGenerator.Services.Inner;
 
 namespace SongkickSpotifyPlaylistGenerator.Services
